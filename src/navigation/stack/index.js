5951b7e96eb7c1437183ca67e9dcf2b0b085e424
Yevhen <eug.z2004@gmail.com>
2022-12-18 15:55:41 +0200

    added icons to tabs

--- src/navigation/stack/index.js
+++ src/navigation/stack/index.js
@@ -2,12 +2,29 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {HomeStackScreen} from './homeStack';
 import {InfoScreen} from '../screens/info';
+import {Text} from 'react-native';
+import Home from '../../assets/svg/home.svg';
+import Settings from '../../assets/svg/settings.svg';
 
 const Tab = createBottomTabNavigator();
 
 export const MainStack = () => {
   return (
-    <Tab.Navigator screenOptions={{headerShown: false}}>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        headerShown: false,
+        tabBarIcon: ({color, size}) => {
+          switch (route.name) {
+            case 'Home':
+              return <Home height={20} width={20} fill={color} />;
+            case 'Info':
+              return <Settings height={20} width={20} fill={color} />;
+            default:
+              return <Settings height={20} width={20} fill={color} />;
+          }
+        },
+        tabBarActiveTintColor: '#FF6B08',
+      })}>
       <Tab.Screen name="Home" component={HomeStackScreen} />
       <Tab.Screen name="Info" component={InfoScreen} />
     </Tab.Navigator>
