729942ee0e3cc7005064c985c6aacccd8b03d467
Yevhen <eug.z2004@gmail.com>
2022-12-24 13:07:31 +0200

    added custom font

--- src/navigation/screens/home/styles.js
+++ src/navigation/screens/home/styles.js
@@ -3,10 +3,13 @@ import {StyleSheet} from 'react-native';
 export const styles = StyleSheet.create({
   flex: {
     flex: 1,
-    backgroundColor: 'red',
   },
   container: {
     flex: 1,
     padding: 16,
+  },
+  text: {
+    fontFamily: 'Roboto-Regular',
+    color: '#303030',
   },
 });
