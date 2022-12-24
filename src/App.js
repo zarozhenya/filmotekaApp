2476456913985794f68d760e6f0350bb64afee1a
Yevhen <eug.z2004@gmail.com>
2022-12-24 14:02:58 +0200

    added custom variables for accessing movies api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {useEffect} from 'react';
 import SplashScreen from 'react-native-splash-screen';
 import {Navigation} from './navigation';
+import Config from 'react-native-config';
 
 const App = () => {
   useEffect(() => {
     SplashScreen.hide();
+    console.log(Config.API_URL, Config.API_KEY);
   }, []);
   return <Navigation />;
 };
